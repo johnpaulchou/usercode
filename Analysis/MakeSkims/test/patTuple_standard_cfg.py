4639b5553a48fb83a5adfe2a0bfe1b6c0b67ed82
johnpaul <>
2010-07-22 10:33:05 +0000

    update

--- Analysis/MakeSkims/test/patTuple_standard_cfg.py
+++ Analysis/MakeSkims/test/patTuple_standard_cfg.py
@@ -1,47 +1,32 @@
 ## import skeleton process
 from PhysicsTools.PatAlgos.patTemplate_cfg import *
 
+## import tools
+from PhysicsTools.PatAlgos.tools.coreTools import *
 
+## this is the dataset we're going to run on
+import Analysis.MakeSkims.dataset_JetMETTauJuly15thReReco_cfi as dataset
+#import Analysis.MakeSkims.dataset_ZtautauM20CTEQ66Summer10_cfi as dataset
 
-## ------------------------------------------------------
-#  NOTE: you can use a bunch of core tools of PAT to
-#  taylor your PAT configuration; for a few examples
-#  uncomment the lines below
-## ------------------------------------------------------
-#from PhysicsTools.PatAlgos.tools.coreTools import *
-
-## remove MC matching from the default sequence
-# removeMCMatching(process, ['Muons'])
-
-## remove certain objects from the default sequence
-# removeAllPATObjectsBut(process, ['Muons'])
-# removeSpecificPATObjects(process, ['Electrons', 'Muons', 'Taus'])
-
-## let it run
-process.p = cms.Path(
-    process.patDefaultSequence
-    )
-
-
-# this is the dataset we're going to run on
-#import Analysis.MakeSkims.dataset_JetMETTauJuly15thReReco_cfi as dataset
-import Analysis.MakeSkims.dataset_ZtautauSummer10_cfi as dataset
-
-# link everything up here
+## link everything up here
 process.GlobalTag.globaltag = dataset.globaltag
 process.source.fileNames = dataset.readFiles
 process.out.fileName = dataset.outfile
 process.source.lumisToProcess = dataset.lumisToProcess
 
-# if we're running on data remove MC matching
+## if we're running on data remove MC matching
 if not dataset.isMC:
-    from PhysicsTools.PatAlgos.tools.coreTools import *
     removeMCMatching(process)
 
-# turn off JECs (for now)
-#process.allLayer1Jets.addJetCorrFactors = False
+## Filter out events
+process.load('Analysis.MakeSkims.custom_filters_cfi')
+process.out.SelectEvents = cms.untracked.PSet ( SelectEvents = cms.vstring('p') )
 
-# reset other options
+## set other running options
 process.options.wantSummary = True
+process.MessageLogger.cerr.FwkReport.reportEvery = 1000
 process.maxEvents.input = 100
 #   process.out.outputCommands = [ ... ]  ##  (e.g. taken from PhysicsTools/PatAlgos/python/patEventContent_cff.py)
+
+## let it run
+process.p = cms.Path(process.customFilterSequence*process.patDefaultSequence)
