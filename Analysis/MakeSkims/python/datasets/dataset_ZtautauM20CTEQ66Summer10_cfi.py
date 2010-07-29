b60eadd0015c55f904f42c71fd763b3edc291bac
johnpaul <>
2010-07-29 10:44:30 +0000

    moved files

---
+++ Analysis/MakeSkims/python/datasets/dataset_ZtautauM20CTEQ66Summer10_cfi.py
@@ -0,0 +1,23 @@
+import FWCore.ParameterSet.Config as cms
+
+## production release = CMSSW_3_6_2
+
+## dataset = /Ztautau_M20_CTEQ66-powheg/Summer10-START36_V9_S09-v1/GEN-SIM-RECO
+
+## global tag
+globaltag = cms.string('START36_V9::All')
+
+## is MC
+isMC = True
+
+## read files (no files are found locally)
+readFiles = cms.untracked.vstring('/store/mc/Summer10/Ztautau_M20_CTEQ66-powheg/GEN-SIM-RECO/START36_V9_S09-v1/0026/02579C1B-E276-DF11-949B-00261894380D.root')
+readFiles = cms.untracked.vstring('/store/mc/Summer10/Ztautau_M20_CTEQ66-powheg/GEN-SIM-RECO/START36_V9_S09-v1/0026/064E0F28-E576-DF11-8DA1-002618943910.root')
+readFiles = cms.untracked.vstring('/store/mc/Summer10/Ztautau_M20_CTEQ66-powheg/GEN-SIM-RECO/START36_V9_S09-v1/0026/0C8B783B-E976-DF11-8DD3-003048678FDE.root')
+readFiles = cms.untracked.vstring('/store/mc/Summer10/Ztautau_M20_CTEQ66-powheg/GEN-SIM-RECO/START36_V9_S09-v1/0026/16F267BB-E576-DF11-9AE4-002618943920.root')
+
+## out files
+outfile = cms.untracked.string('ZtautauM20CTEQ66Summer10.root')
+
+## good run list
+lumisToProcess = cms.untracked.VLuminosityBlockRange()
