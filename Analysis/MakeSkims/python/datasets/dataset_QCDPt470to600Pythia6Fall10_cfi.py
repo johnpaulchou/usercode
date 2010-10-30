cd4464377e4014169bdaa7895f27d9ecb5e3112c
johnpaul <>
2010-10-30 20:28:31 +0000

    added Pythia6 datasets

---
+++ Analysis/MakeSkims/python/datasets/dataset_QCDPt470to600Pythia6Fall10_cfi.py
@@ -0,0 +1,24 @@
+import FWCore.ParameterSet.Config as cms
+
+## production release = CMSSW_3_8_4_patch2
+
+dataset = '/QCD_Pt_470to600_TuneZ2_7TeV_pythia6/Fall10-START38_V12-v1/GEN-SIM-RECO'
+
+## global tag
+globaltag = cms.string('START38_V12::All')
+
+## is MC
+isMC = True
+
+## read files
+readFiles = cms.untracked.vstring()
+
+## out files
+outfile = cms.untracked.string('QCDPt470to600Pythia6Fall10.root')
+
+## good run list
+lumisToProcess = cms.untracked.VLuminosityBlockRange()
+
+## do other stuff
+def doOtherStuff(process):
+    pass
---
+++ Analysis/MakeSkims/python/datasets/dataset_QCDPt600to800Pythia6Fall10_cfi.py
@@ -0,0 +1,24 @@
+import FWCore.ParameterSet.Config as cms
+
+## production release = CMSSW_3_8_4_patch2
+
+dataset = '/QCD_Pt_600to800_TuneZ2_7TeV_pythia6/Fall10-START38_V12-v2/GEN-SIM-RECO'
+
+## global tag
+globaltag = cms.string('START38_V12::All')
+
+## is MC
+isMC = True
+
+## read files
+readFiles = cms.untracked.vstring()
+
+## out files
+outfile = cms.untracked.string('QCDPt600to800Pythia6Fall10.root')
+
+## good run list
+lumisToProcess = cms.untracked.VLuminosityBlockRange()
+
+## do other stuff
+def doOtherStuff(process):
+    pass
