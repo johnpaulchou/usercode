2bc50133895f11e4bbf90e7ebf4197b250dc3af9
johnpaul <>
2010-11-14 00:02:55 +0000

    fixed bugs and updated scripts

--- ADDDiPhoton/scripts/SherpaGen/createADDDiPhotonLib.sh
+++ ADDDiPhoton/scripts/SherpaGen/createADDDiPhotonLib.sh
@@ -1,89 +1,59 @@
 #!/bin/tcsh -f
-#usesage ./condor_execute_manual.sh MSSCALE NED KKCONVENTION LOWMASSCUT HIGHMASSCUT 
+
 #===========Input parameter===================
-set VERSION="3_9_2"
-set VERNAME="3_9_2"
-set PRENAME="ADDdiphoton_gen"
-set CURRENTDIR="SherpaGenADDphoton"
+set LIBNAME="ADDDiPhoton"
+set CONDITIONS="MC_39Y_V5"
+set NEVENTS=20
+
 set MSSCALE=$1
 set NED=$2
 set KKCONVENTION=$3
 set LOWMASSCUT=$4
 set HIGHMASSCUT=$5
-set PROCESS = "${PRENAME}_MS${MSSCALE}_NED${NED}_KK${KKCONVENTION}_MASSCUT_${LOWMASSCUT}_${HIGHMASSCUT}"
-#======environment setup==========
-unsetenv CMS_PATH
-source /uscmst1/prod/sw/cms/cshrc prod
-cd /uscms/home/johnpaul/cmssw/CMSSW_${VERSION}/src
-pwd
-eval `scramv1 runtime -csh`
-cd $CURRENTDIR
-pwd
+set RANDOMSEED=$6
+set PROCESS = "MS${MSSCALE}_NED${NED}_KK${KKCONVENTION}_MASSCUT_${LOWMASSCUT}_${HIGHMASSCUT}"
 
 #======create library==========
-set WORKDIR=$PROCESS
-set subWORKDIR="test"
-rm -rf $WORKDIR
-mkdir $WORKDIR
-cd $WORKDIR
-rm Run.dat
+set LIBPATH=${CMSSW_BASE}/src/${LIBNAME}
+set WORKPATH=${LIBPATH}/$PROCESS
+rm -rf $WORKPATH
+mkdir -p $WORKPATH
 
-cat ../Run.dat | sed -e s/EVENTNUM/100/ | sed -e s/NED/$NED/ | sed -e s/MSSCALE/$MSSCALE/ | sed -e s/KKCONVENTION/$KKCONVENTION/ | sed -e s/LOWMASSCUT/$LOWMASSCUT/ | sed -e s/HIGHMASSCUT/$HIGHMASSCUT/ > Run.dat
-cp ../Analysis.dat .
-tar -czvf sherpa_${PROCESS}_cards.tgz Run.dat
+# copy files to the workpath
+set DATFILE=ADDDiPhoton.dat
+set ANAFILE=${CMSSW_RELEASE_BASE}/src/GeneratorInterface/SherpaInterface/data/Analysis.dat
+set MAKELIBSFILE=MakeSherpaLibs.sh
+set PREPLIBSFILE=PrepareSherpaLibs.sh
 
-cp ../MakeSherpaLibs.sh .
-cp ../PrepareSherpaLibs.sh .
-#./MakeSherpaLibs.sh -d $CMSSW_BASE -i ./ -p $PROCESS
+cat ${DATFILE} | sed -e s/EVENTNUM/${NEVENTS}/ | sed -e s/NED/$NED/ | sed -e s/MSSCALE/$MSSCALE/ | sed -e s/KKCONVENTION/$KKCONVENTION/ | sed -e s/LOWMASSCUT/$LOWMASSCUT/ | sed -e s/HIGHMASSCUT/$HIGHMASSCUT/ > ${WORKPATH}/Run.dat
+cp ${ANAFILE} ${WORKPATH}/Analysis.dat
+cp ${MAKELIBSFILE} ${WORKPATH}/MakeSherpaLibs.sh
+cp ${PREPLIBSFILE} ${WORKPATH}/PrepareSherpaLibs.sh
+
+# make the library
+cd ${WORKPATH}
+tar -czf sherpa_${PROCESS}_cards.tgz Run.dat Analysis.dat
 ./MakeSherpaLibs.sh -i ./ -p $PROCESS
-#mv sherpa_${PROCESS}_crdL.tgz sherpa_${PROCESS}_crdE.tgz
-
-#mkdir tmp
-#mv sherpa_${PROCESS}_crdE.tgz tmp
-#cd tmp
-#tar -xzvf sherpa_${PROCESS}_crdE.tgz
-#rm sherpa_${PROCESS}_crdE.tgz
-#cat ../../Run.dat | sed -e s/EVENTNUM/100000/ | sed -e s/NED/$NED/ | sed -e s/MSSCALE/$MSSCALE/ | sed -e s/KKCONVENTION/$KKCONVENTION/ | sed -e s/LOWMASSCUT/$LOWMASSCUT/ | sed -e s/HIGHMASSCUT/$HIGHMASSCUT/ > Run.dat
-#tar -czvf sherpa_${PROCESS}_crdE.tgz *.*
-#mv sherpa_${PROCESS}_crdE.tgz ../
-#cd ../
-#tar -czvf sherpa_${PROCESS}_cards.tgz Run.dat
-
-./PrepareSherpaLibs.sh -i ./ -p $PROCESS -c MC_39Y_V5 -m PROD
-
+./PrepareSherpaLibs.sh -i ./ -p $PROCESS -c ${CONDITIONS} -m PROD
 tar -xzf sherpa_${PROCESS}_MASTER.tgz
 mkdir python
 cp sherpa_${PROCESS}_cff.py sherpa_custom.py python
-scramv1 b
+scram b
 
-#./PrepareSherpaLibs.sh -d $CMSSW_BASE -i ./ -p $PROCESS -a SherpaGenADDelectron/$WORKDIR -m LOCAL
-#mkdir test
-#cp sherpa_${PROCESS}_MASTER.tgz test
-#cd test
-#tar -xzf sherpa_${PROCESS}_MASTER.tgz
-#mkdir ../python
-#cp sherpa_custom.py ../python
-#cp sherpa_${PROCESS}_cff.py ../python
-#cd ../python
-#scramv1 b
-#cd ../test
-#echo $PWD
+# create cmsRun config script
+cmsDriver.py $LIBNAME/$PROCESS/sherpa_${PROCESS}_cff.py -s GEN,SIM,DIGI,L1,DIGI2RAW,RAW2DIGI,RECO --conditions ${CONDITIONS}::All --datatier GEN-SIM-RECO --eventcontent RECOSIM  --customise $LIBNAME/$PROCESS/sherpa_custom.py -n ${NEVENTS} --no_exec --fileout output_${PROCESS}.root
 
-#cmsDriver.py $CURRENTDIR/$WORKDIR/sherpa_${PROCESS}_cff.py -s GEN,SIM,DIGI,L1,DIGI2RAW,HLT --conditions MC_38Y_V8::All --datatier GEN-SIM-RAW --eventcontent RAWSIM  --customise $CURRENTDIR/$WORKDIR/sherpa_custom.py -n 20 --no_exec --fileout outputGENDIGIHLT_${PROCESS}.root
-cmsDriver.py $CURRENTDIR/$WORKDIR/sherpa_${PROCESS}_cff.py -s GEN,SIM,DIGI,L1,DIGI2RAW,RAW2DIGI,RECO --conditions MC_39Y_V5::All --datatier GEN-SIM-RECO --eventcontent RECOSIM  --customise $CURRENTDIR/$WORKDIR/sherpa_custom.py -n 10000 --no_exec --fileout output_${PROCESS}.root
+# modify config script
+echo "process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )" > modified.py
+echo "process.RandomNumberGeneratorService = cms.Service('RandomNumberGeneratorService'," >> modified.py
+echo "   moduleSeeds = cms.PSet(generator = cms.untracked.uint32(${RANDOMSEED})))" >> modified.py
 
-cat sherpa_${PROCESS}_cff_py_GEN_SIM_DIGI_L1_DIGI2RAW_RAW2DIGI_RECO.py ../modified.py > runSherpa.py
+cat sherpa_${PROCESS}_cff_py_GEN_SIM_DIGI_L1_DIGI2RAW_RAW2DIGI_RECO.py modified.py > runSherpa.py
 
-#cmsRun runSherpa.py > log_$PROCESS
-
-#cp log_$PROCESS ../Log
-
+# final result
 echo "MS, NED, KK, LOWMASS, HIGHMASS:"
 echo "MS: " ${MSSCALE}
 echo "NED: " ${NED}
 echo "KKCONVENTION: " ${KKCONVENTION}
 echo "LOWMASSCUT: " ${LOWMASSCUT}
 echo "HIGHMASSCUT: " ${HIGHMASSCUT}
-
-#process.generator.SherpaParameters.Run[1] = 'EVENTS = 100000'
-#process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) ) 
--- ADDDiPhoton/scripts/SherpaGen/runCreateADDDiPhotonLib.sh
+++ ADDDiPhoton/scripts/SherpaGen/runCreateADDDiPhotonLib.sh
@@ -1,35 +1,35 @@
 #!/bin/tcsh -f
 
-./condor_execute_manual.sh 100000 4 1 200 7000
+./createADDDiPhotonLib.sh 100000 4 1 200 7000
 
-./condor_execute_manual.sh 1200 5 1 200 7000
-./condor_execute_manual.sh 1200 7 1 200 7000
+#./createADDDiPhotonLib.sh 1200 5 1 200 7000
+#./createADDDiPhotonLib.sh 1200 7 1 200 7000
 
-./condor_execute_manual.sh 1500 2 1 200 7000
-./condor_execute_manual.sh 1500 3 1 200 7000
-./condor_execute_manual.sh 1500 4 1 200 7000
-./condor_execute_manual.sh 1500 5 1 200 7000
-./condor_execute_manual.sh 1500 6 1 200 7000
-./condor_execute_manual.sh 1500 7 1 200 7000
+#./createADDDiPhotonLib.sh 1500 2 1 200 7000
+#./createADDDiPhotonLib.sh 1500 3 1 200 7000
+#./createADDDiPhotonLib.sh 1500 4 1 200 7000
+#./createADDDiPhotonLib.sh 1500 5 1 200 7000
+#./createADDDiPhotonLib.sh 1500 6 1 200 7000
+#./createADDDiPhotonLib.sh 1500 7 1 200 7000
 
-./condor_execute_manual.sh 2000 2 1 200 7000
-./condor_execute_manual.sh 2000 3 1 200 7000
-./condor_execute_manual.sh 2000 4 1 200 7000
-./condor_execute_manual.sh 2000 7 1 200 7000
+#./createADDDiPhotonLib.sh 2000 2 1 200 7000
+#./createADDDiPhotonLib.sh 2000 3 1 200 7000
+#./createADDDiPhotonLib.sh 2000 4 1 200 7000
+#./createADDDiPhotonLib.sh 2000 7 1 200 7000
 
-./condor_execute_manual.sh 2500 2 1 200 7000
-./condor_execute_manual.sh 2500 3 1 200 7000
-./condor_execute_manual.sh 2500 4 1 200 7000
-./condor_execute_manual.sh 2500 7 1 200 7000
+#./createADDDiPhotonLib.sh 2500 2 1 200 7000
+#./createADDDiPhotonLib.sh 2500 3 1 200 7000
+#./createADDDiPhotonLib.sh 2500 4 1 200 7000
+#./createADDDiPhotonLib.sh 2500 7 1 200 7000
 
-./condor_execute_manual.sh 3000 2 1 200 7000
-./condor_execute_manual.sh 3000 4 1 200 7000
-./condor_execute_manual.sh 3000 6 1 200 7000
+#./createADDDiPhotonLib.sh 3000 2 1 200 7000
+#./createADDDiPhotonLib.sh 3000 4 1 200 7000
+#./createADDDiPhotonLib.sh 3000 6 1 200 7000
 
-./condor_execute_manual.sh 4000 2 1 200 7000
-./condor_execute_manual.sh 4000 4 1 200 7000
-./condor_execute_manual.sh 4000 6 1 200 7000
+#./createADDDiPhotonLib.sh 4000 2 1 200 7000
+#./createADDDiPhotonLib.sh 4000 4 1 200 7000
+#./createADDDiPhotonLib.sh 4000 6 1 200 7000
 
-./condor_execute_manual.sh 5000 2 1 200 7000
-./condor_execute_manual.sh 5000 4 1 200 7000
-./condor_execute_manual.sh 5000 6 1 200 7000
+#./createADDDiPhotonLib.sh 5000 2 1 200 7000
+#./createADDDiPhotonLib.sh 5000 4 1 200 7000
+#./createADDDiPhotonLib.sh 5000 6 1 200 7000
